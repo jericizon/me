57ece48d47fb3d95db38571339b75f13c9a75900
jericizon <im.jericizon@gmail.com>
2024-02-15 00:00:10 +0800

    updates

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup>
+useSeoMeta({
+  // meta title and description should be in content/licens-agreement.md
+  ogTitle: "Jeric Izon - Freelance website developer.",
+  ogType: "website",
+  // ogImage: '/images/home/main.jpg',
+  // ogDescription: '',
+  ogUrl: useRoute()?.path,
+});
+</script>
 <template>
-  <div>
-    <Banner />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
